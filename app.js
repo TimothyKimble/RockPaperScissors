6e82f9567c6db8ed5720704f8d0c5d7ae156868e
Timothy Kimble <timble417@gmail.com>
2021-07-01 17:54:19 -0600

    worked on computer vs player select

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function drawCards() {
   for (let key in choices) {
     let choice = choices[key]
     template += `    <div class="card m-2 box-shadow-dark" style="width: 18rem;">
-    <button onclick="pick('${key}')" "computerChoice('${key}')">
+    <button onclick="pick('${key}') , computerSelect('${key}')">
       <img class="card-img-top w-100" src="${choice.img}" alt="${choice.says}">
       <div class="card-body">
         <h5 class="card-title d-flex justify-content-center">${choice.says}</h5>
@@ -31,6 +31,23 @@ function drawCards() {
   </div>`
   }
   document.getElementById('Cards').innerHTML = template
+}
+
+function drawcomputercard() {
+  let template = ''
+  for (let key in choices) {
+    let choice = choices[key]
+    template += `    <div class="card m-2 box-shadow-dark" style="width: 18rem;">
+    <button onclick="computerSelect('${key}')">
+      <img class="card-img-top w-100" src="${choice.img}" alt="${choice.says}">
+      <div class="card-body">
+        <h5 class="card-title d-flex justify-content-center">${choice.says}</h5>
+      </div>
+    </button>
+
+  </div>`
+  }
+  document.getElementById('computer-img').innerHTML = template + Math.random()
 }
 
 function pick(weapon) {
@@ -47,12 +64,47 @@ function pick(weapon) {
     </div>
 
 </div>`
+
+
 }
 
 drawCards()
 
 function computerChoice() {
-  let choice = Object.keys(choices)
-  let randindex = Math.floor(Math.random() * (choice.length))
-  pick(choice[randindex])
+  let choice = choices[Math.floor(Math.random() * choices.length)]
+  console.log('computer', choice)
+  return choice
+
+
+
+}
+
+function play(player, comp) {
+  let battle = choices[player]
+  console.log(battle)
+  if (player == comp) {
+    return 'You tied'
+  } else if (battle[0] == comp) {
+    computerscore++
+    return 'You lost'
+  }
+  playerscore++
+  return "Winner"
+}
+
+function computerSelect(compweapon) {
+  let compchoice = choices[compweapon]
+  console.log(compchoice)
+
+
+  document.getElementById('computer-weapon').innerText = `Computer Choice ${compchoice.says}`
+
+  document.getElementById('computer-img').innerHTML = `<div class="card m-2 box-shadow-dark " style="width: 25rem; height: 25rem;">
+ 
+ <img class="card-img-top w-100" src="${compchoice.img}" alt="${compchoice.says}">
+ <div class="card-body">
+   <h5 class="card-title d-flex justify-content-center">${compchoice.says}</h5>
+ </div>
+
+</div>`
 }
